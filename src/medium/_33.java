c29d0d84c75092834ec173a87f929bb4abc8bd2d
Konstantin <konstantin.kernazhytski@gmail.com>
2025-04-13 21:37:21 +0300

    a few problems solved

---
+++ src/medium/_33.java
@@ -0,0 +1,7 @@
+package medium;
+
+public class _33 {
+    public int search(int[] nums, int target) {
+
+    }
+}
--- src/medium/_424.java
+++ src/medium/_424.java
@@ -1,4 +1,36 @@
 package medium;
 
+import java.util.HashMap;
+import java.util.HashSet;
+
 public class _424 {
+    public int characterReplacement(String s, int k) {
+        HashMap<Character, Integer> map = new HashMap<>();
+        map.put(s.charAt(0), 1);
+
+        int startIndex = 0;
+        int length = 0;
+        int swaps = k;
+
+        for (int i = 1; i < s.length(); i++) {
+
+            map.merge(s.charAt(i), 1, Integer::sum);
+            if (s.charAt(startIndex) != s.charAt(i)) {
+                if (swaps <= 0 ) {
+                    map.merge(s.charAt(startIndex), -1, Integer::sum);
+                    startIndex++;
+                    swaps = k + map.get(s.charAt(startIndex)) + startIndex + 1 - i;
+                } else {
+                    swaps--;
+                }
+            }
+            if (length < i - startIndex ){
+                length = i - startIndex + 1;
+            }
+        }
+
+        return Math.min(length, s.length());
+
+        
+    }
 }
